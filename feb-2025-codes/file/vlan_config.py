3e3b3c92e8830f8253a561f2bdd1ca9ef34ebb9e
aadarshg3 <aadarshg3@gmail.com>
2025-04-12 07:14:31 +0000

    update

---
+++ feb-2025-codes/file/vlan_config.py
@@ -0,0 +1,18 @@
+import time
+from datetime import datetime
+
+# reading from txt file
+myfile = open("vlan_data.txt", "r")
+data = myfile.readlines()
+# print(myfile.readlines())
+# print(data[0:1])
+
+for item in data:
+    print(item.strip("\n"))
+myfile.close()    
+
+
+
+myfile = open(f"vlan_data{datetime.now()}.txt", "w", newline='\n')
+p_data = "This is Aadarsh from NetGIndia"
+myfile.write(p_data)
