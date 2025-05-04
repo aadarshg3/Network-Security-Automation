93a53d9a29b03d075800cf14fdae02fdbb3744ce
aadarshg3 <aadarshg3@gmail.com>
2025-05-04 04:16:40 +0000

    netmiko

--- feb-2025-codes/file/json-yaml-xml/jinja2/1.py
+++ feb-2025-codes/file/json-yaml-xml/jinja2/1.py
@@ -5,8 +5,8 @@ from jinja2 import FileSystemLoader, Environment
 with open("intf_config_data.yaml", "r") as intef_data_yaml_file:
     sw_config_data = yaml.safe_load(intef_data_yaml_file)  # Read YAML and convert into a Python list of dicts
 
-print("Loaded YAML Data:")
-print(sw_config_data)
+# print("Loaded YAML Data:")
+# print(sw_config_data)
 
 # Set up the Jinja2 environment
 template_loader = FileSystemLoader(searchpath=".")
@@ -21,3 +21,36 @@ config_list = rendered_output.splitlines()
 print("\nRendered Configuration:")
 for line in config_list:
     print(line)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import yaml  # Import YAML module
+from jinja2 import FileSystemLoader, Environment  # Import Jinja2 classes
+
+# Load YAML file safely using 'with'
+with open("vlan.yaml") as yaml_file:
+    sw_config_data = yaml.safe_load(yaml_file)  # Parse YAML to dict
+
+# Set up Jinja2 environment and load the correct template file
+template_loader = FileSystemLoader(searchpath=".")
+env = Environment(loader=template_loader)
+template = env.get_template("vlan.j2")  # Correct filename
+
+# Render template by passing the full YAML dict as 'device_data'
+rendered_output = template.render(device_data=sw_config_data)
+
+# Print the final configuration
+print(rendered_output)
